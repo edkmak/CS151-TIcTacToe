79cbc7a251cee597388bd74fa9c8530b010cd017
jmelberg <jordan.melberg@gmail.com>
2015-12-01 18:39:57 -0800

    added drawx and drawo to cell

--- Cell.java
+++ Cell.java
@@ -1,27 +1,49 @@
 /* Cell class */
 package Project;
 
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
 public class Cell {
 
   public static final int MIN = 1;
   public static final int MAX = 3;
+  private ImageIcon xIcon = new ImageIcon("images/x.png"));
+  private ImageIcon oIcon = new ImageIcon("images/o.png"));
+  
   private int x, y;
   boolean taken;
 
-  // Constructor to assign value to cell
-  public Cell(int x, int y){
+  // Constructor to create cell
+  public Cell(){
+    this.x = 0;
+    this.y = 0;
+    taken = false;
+  }
 
-    if( x<MIN | x >MAX | y<MIN | y>MAX ){
-      throw new IllegalArgumentException();
+  public int getX(){ return x; }
+  public int getY() { return y; }
+
+  // Methods to assign values to cell
+  public ImageIcon drawX(int x, int y){
+    if (x<MIN | x>MAX | y<MIN | y>MAX){
+      throw new Exception("Input outside of alloted range");
     }
-
     this.x = x;
     this.y = y;
     taken = true;
+    return xIcon;
+  }
+  public ImageIcon drawO(int x, int y){
+    if (x<MIN | x>MAX | y<MIN | y>MAX){
+      throw new Exception("Input outside of alloted range");
+    }
+    this.x = x;
+    this.y = y;
+    taken=true;
+    return oIcon
   }
 
-  public getX(){ return x; }
-  public getY() { return y; }
   public isEmpty() {return taken; }
 
 }
